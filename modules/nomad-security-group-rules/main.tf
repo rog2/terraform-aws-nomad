e12d4cb06b43a579563a46385db21765869fbcaa
Zhiguang Wang <wangzhiguang@kingsoft.com>
2021-06-02 15:25:43 +0800

    Resolve error "cidr_blocks conflicts with self" since terraform-provider-aws 3.38.0
    https://github.com/hashicorp/terraform-provider-aws/pull/18467

--- modules/nomad-security-group-rules/main.tf
+++ modules/nomad-security-group-rules/main.tf
@@ -16,7 +16,6 @@ resource "aws_security_group_rule" "allow_http_inbound" {
   to_port     = var.http_port
   protocol    = "tcp"
   cidr_blocks = var.allowed_inbound_cidr_blocks
-  self        = true
 
   security_group_id = var.security_group_id
 }
@@ -27,7 +26,6 @@ resource "aws_security_group_rule" "allow_rpc_inbound" {
   to_port     = var.rpc_port
   protocol    = "tcp"
   cidr_blocks = var.allowed_inbound_cidr_blocks
-  self        = true
 
   security_group_id = var.security_group_id
 }
@@ -38,7 +36,6 @@ resource "aws_security_group_rule" "allow_serf_tcp_inbound" {
   to_port     = var.serf_port
   protocol    = "tcp"
   cidr_blocks = var.allowed_inbound_cidr_blocks
-  self        = true
 
   security_group_id = var.security_group_id
 }
@@ -49,8 +46,48 @@ resource "aws_security_group_rule" "allow_serf_udp_inbound" {
   to_port     = var.serf_port
   protocol    = "udp"
   cidr_blocks = var.allowed_inbound_cidr_blocks
-  self        = true
 
   security_group_id = var.security_group_id
 }
 
+# Similar to the allow_*_inbound rules, allow inbound from self
+
+resource "aws_security_group_rule" "allow_http_inbound_from_self" {
+  type      = "ingress"
+  from_port = var.http_port
+  to_port   = var.http_port
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = var.security_group_id
+}
+
+resource "aws_security_group_rule" "allow_rpc_inbound_from_self" {
+  type      = "ingress"
+  from_port = var.rpc_port
+  to_port   = var.rpc_port
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = var.security_group_id
+}
+
+resource "aws_security_group_rule" "allow_serf_tcp_inbound_from_self" {
+  type      = "ingress"
+  from_port = var.serf_port
+  to_port   = var.serf_port
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = var.security_group_id
+}
+
+resource "aws_security_group_rule" "allow_serf_udp_inbound_from_self" {
+  type      = "ingress"
+  from_port = var.serf_port
+  to_port   = var.serf_port
+  protocol  = "udp"
+  self      = true
+
+  security_group_id = var.security_group_id
+}
